4fabcb18bacb548dbb70e0be2b9a728afc388b89
Tigran <tig.vardanyan.001@gmail.com>
2020-09-25 04:30:58 +0400

    Fetching Orders

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import classes from './Logo.css';
 
 import burgerLogo from '../../assets/burger-logo.png'
 
 const logo = (props) => (
   <div className={classes.Logo}>
-    <img src={burgerLogo} alt=""/>
+    <Link to={'/'} >
+      <img src={burgerLogo} alt=""/>
+    </Link>
   </div>
 )
 
